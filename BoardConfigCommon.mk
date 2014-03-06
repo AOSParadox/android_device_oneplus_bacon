2326c79e1a01743f9a1f2723f88e8deea5bd7852
Ethan Chen <intervigil@gmail.com>
2014-03-05 20:11:54 -0800

    msm8974-common: Add KeyDisabler
    Change-Id: Ic8b3b7b14fc6919ede9dc84d7cf68447dd62702e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,9 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 # Charger
 BOARD_CHARGER_RES := device/oppo/msm8974-common/charger/images
 
+# CM Hardware
+BOARD_HARDWARE_CLASS += device/oppo/msm8974-common/cmhw
+
 # SELinux
 BOARD_SEPOLICY_DIRS := \
    device/oppo/msm8974-common/sepolicy
