3b755ce0e40b0325c233a7dbbecbc78fdaca82c8
Steve Kondik <shade@chemlab.org>
2014-03-06 21:49:33 -0800

    msm8974: Update blobstuffs
     * Add ADSP blobs and fix extract path
    
    Change-Id: Ibe54aeec351b94f9366492526492119b3552eff3

--- extract-files.sh
+++ extract-files.sh
@@ -2,28 +2,36 @@
 
 set -e
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+        DIR=`dirname $FILE`
+        if [ ! -d $2/$DIR ]; then
+            mkdir -p $2/$DIR
+        fi
+        # Try CM target first
+        adb pull /system/$DEST $2/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $2/$DEST
+        fi
+    done
+}
+
+
+BASE=../../../vendor/oppo/msm8974-common/proprietary
 rm -rf $BASE/*
 
-for FILE in `egrep -v '(^#|^$)' ../../oppo/msm8974-common/proprietary-files.txt` `egrep -v '(^#|^$)' ../../oppo/msm8974-common/proprietary-files-qc.txt`; do
-  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-  FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-  DEST=${PARSING_ARRAY[1]}
-  if [ -z $DEST ]
-  then
-    DEST=$FILE
-  fi
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  # Try CM target first
-  adb pull /system/$DEST $BASE/$DEST
-  # if file does not exist try OEM target
-  if [ "$?" != "0" ]
-  then
-    adb pull /system/$FILE $BASE/$DEST
-  fi
-done
+DEVBASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $DEVBASE/*
+
+extract ../../oppo/msm8974-common/proprietary-files-qc.txt $BASE
+extract ../../oppo/msm8974-common/proprietary-files.txt $DEVBASE
+extract ../../$VENDOR/$DEVICE/device-proprietary-files.txt $DEVBASE
 
 ./../../oppo/msm8974-common/setup-makefiles.sh
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -156,29 +156,7 @@ for FILE in `egrep -v '(^#|^$)' ../../oppo/msm8974-common/proprietary-files-qc.t
 done
 (cat << EOF) >> $MAKEFILE
 endif
-
-PRODUCT_COPY_FILES += \\
 EOF
-LINEEND=" \\"
-COUNT=`wc -l ../../oppo/msm8974-common/common-proprietary-files.txt | awk {'print $1'}`
-DISM=`egrep -c '(^#|^$)' ../../oppo/msm8974-common/common-proprietary-files.txt`
-COUNT=`expr $COUNT - $DISM`
-for FILE in `egrep -v '(^#|^$)' ../../oppo/msm8974-common/common-proprietary-files.txt`; do
-  COUNT=`expr $COUNT - 1`
-  if [ $COUNT = "0" ]; then
-    LINEEND=""
-  fi
-  # Split the file from the destination (format is "file[:destination]")
-  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-  if [[ ! "$FILE" =~ ^-.* ]]; then
-    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-    DEST=${PARSING_ARRAY[1]}
-    if [ -n "$DEST" ]; then
-        FILE=$DEST
-    fi
-    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
-  fi
-done
 
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
 # Copyright (C) 2014 The CyanogenMod Project
