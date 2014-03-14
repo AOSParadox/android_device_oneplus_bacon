039e83017046a43e49adb40020943d36a9796448
Steve Kondik <shade@chemlab.org>
2014-03-13 17:56:54 -0700

    msm8974: Quiet build system warnings
    Change-Id: Ic61c3ec3f89e450c6169826cbdd48ce6f1cd72e2

--- Android.mk
+++ Android.mk
@@ -164,9 +164,9 @@ ALL_DEFAULT_INSTALLED_MODULES += $(WV_SYMLINKS)
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
 	ln -sf /data/misc/audio/wcd9320_anc.bin \
 		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_anc.bin;\
-	ln -s /data/misc/audio/mbhc.bin \
+	ln -sf /data/misc/audio/mbhc.bin \
 		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_mbhc.bin; \
-	ln -s /data/misc/audio/wcd9320_mad_audio.bin \
+	ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
 		$(TARGET_OUT)/etc/firmware/wcd9320/wcd9320_mad_audio.bin)
 
 endif
