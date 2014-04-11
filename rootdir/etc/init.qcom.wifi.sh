fc0d1343dd42a2ab2aaafccaad7fa6ecb8313ed9
Ethan Chen <intervigil@gmail.com>
2014-04-10 18:55:16 -0700

    msm8974-common: wlan.driver.config is set by wcnss_service
    Change-Id: I1c129ac11592cfdc9fea3da16295840a411f935c

--- rootdir/etc/init.qcom.wifi.sh
+++ rootdir/etc/init.qcom.wifi.sh
@@ -31,7 +31,6 @@ rm /data/misc/wifi/WCNSS_qcom_cfg.ini 2> /dev/null
 rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin 2> /dev/null
 
 echo `getprop ro.serialno` > /sys/devices/fb000000.qcom,wcnss-wlan/serial_number
-setprop wlan.driver.config /data/misc/wifi/WCNSS_qcom_cfg.ini
 
 logwrapper /system/bin/conn_init
 
