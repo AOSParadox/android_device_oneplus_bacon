8f1ba223de30bda49a48c9bd8dd50cf815636454
Ethan Chen <intervigil@gmail.com>
2014-05-07 21:40:07 -0700

    msm8974-common: Fix typo in makefile
    Change-Id: I82086da19e97be7765d75847bb3c6c81a61428b6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 
 # QCOM config scripts
 
--include $(CLEAR_VARS)
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
