dc139ab83245565370b288f5de46df52df45db48
Ethan Chen <intervigil@gmail.com>
2014-06-10 14:18:35 -0700

    msm8974-common: Update kernel source path
    Change-Id: I4a641b3d0fb4554b88264084a14a0cb24776ec24

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_KERNEL_SEPARATED_DT := true
-TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
+TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 TARGET_CUSTOM_DTBTOOL := dtbToolCM
 
 # Flags
