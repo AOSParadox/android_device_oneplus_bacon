3cdfbb3701e3111024f055b0ccf57ff6eb5c4b57
Steve Kondik <shade@chemlab.org>
2014-06-12 12:43:31 -0700

    msm8974: Enable PCM offload property
    Change-Id: I523afe521fc2dc2f19f458298d0275a0be36ff50

--- msm8974.mk
+++ msm8974.mk
@@ -78,7 +78,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.gapless.enabled=false \
     qcom.hw.aac.encoder=true \
     tunnel.audio.encode=true \
-    media.aac_51_output_enabled=true
+    media.aac_51_output_enabled=true \
+    audio.offload.pcm.enable=true
 
 # Charger
 PRODUCT_PACKAGES += \
