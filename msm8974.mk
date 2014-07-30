1aee35a210c6b1330a11d2ba826ceb5581e21bad
Tom Marshall <tdm@cyngn.com>
2014-07-29 22:26:33 -0700

    Make mixer_paths.xml device specific
     * Make mixer_paths.xml device specific because Bacon and find7 have
       different mic configurations.
    
     * This will allow speaker phone on find7.
    
    Change-Id: Icdb2a5623256386b3cba1a9c939fc795e8fc1125

--- msm8974.mk
+++ msm8974.mk
@@ -41,7 +41,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/acdb/MTP_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/MTP_General_cal.acdb:system/etc/acdbdata/MTP/MTP_General_cal.acdb \
