25cc4ce63ebe8963de72c2adb1b15a726b25b98a
Steve Kondik <shade@chemlab.org>
2014-08-23 22:37:05 -0700

    msm8974: Move QC location provider in priv-app
    Change-Id: I70fe034128c04ddf2d943da731f26359648d4127

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -247,11 +247,12 @@ ifeq (\$(QCPATH),)
 include \$(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
 LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location.apk
+LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
