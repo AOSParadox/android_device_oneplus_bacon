257b1ee8b9dddff12bbcf3fd64687b82570d7cc3
Ajay Kumar <ajayku@codeaurora.org>
2014-09-16 06:37:08 +0000

    bacon: set MAX_ACL_CONNECTIONS to 7
    * As per bluetooth specification stack can support max 7 devices
    so restricting the MAX_ACL_CONNECTIONS that HOST can support to 7

--- bdroid_buildcfg.h
+++ bdroid_buildcfg.h
@@ -24,6 +24,7 @@
 #define BTM_DEF_LOCAL_NAME   "OnePlus One"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+#define MAX_ACL_CONNECTIONS 7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
