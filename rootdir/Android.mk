7335bca709185ea7466c2c853375d0cee66f0815
Scott Mertz <scott@cyngn.com>
2014-09-22 16:04:29 -0700

    Remove CNE init script from eng builds
    This causes horrible logcat output because
    of missing libNimsWrap.so
    
    Change-Id: I30c9695e875937f8865f35a7c8c5e22725ddabc0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,7 +38,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.cne.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.cne.rc
 include $(BUILD_PREBUILT)
