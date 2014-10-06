994260e0999c0139a7b7484dcb6d254f68bcd677
Ethan Chen <intervigil@gmail.com>
2014-10-06 15:27:40 -0700

    msm8974-common: Move kernel commandline to device
    Change-Id: I157258e7ca4cb68a2473785f4151827536d2a0b4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,6 @@ TARGET_CPU_VARIANT := krait
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
