fcb340ec20c542e15124a85d9779dec035d85f5d
Michael Bestas <mikeioannina@gmail.com>
2014-10-14 03:44:33 +0300

    msm8974: lights: Cleanup light state handling
    Change-Id: I62bace34784c7a8e79966861126b06f2f75354d7

--- liblight/lights.c
+++ liblight/lights.c
@@ -299,17 +299,13 @@ static void
 handle_speaker_battery_locked(struct light_device_t *dev,
         const struct light_state_t *state)
 {
+    set_speaker_light_locked(dev, NULL);
     if (is_lit(&g_attention)) {
-        set_speaker_light_locked(dev, NULL);
         set_speaker_light_locked(dev, &g_attention);
-    } else if (is_lit(&g_battery) && is_lit(&g_notification)) {
-        set_speaker_light_locked(dev, NULL);
+    } else if (is_lit(&g_notification)) {
         set_speaker_light_locked(dev, &g_notification);
-    } else if (is_lit(&g_battery)) {
-        set_speaker_light_locked(dev, NULL);
-        set_speaker_light_locked(dev, &g_battery);
     } else {
-        set_speaker_light_locked(dev, &g_notification);
+        set_speaker_light_locked(dev, &g_battery);
     }
 }
 
