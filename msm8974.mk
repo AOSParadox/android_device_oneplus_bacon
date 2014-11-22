aa029fcf2da29b8e61074961d13f84a16d23dd24
Steve Kondik <steve@cyngn.com>
2014-11-22 03:53:21 -0800

    Revert "msm8974: Use AwesomePlayer by default"
    This reverts commit dd4854f4828fe4788177aaaa9fff7de7dd90cd17.
    
    Change-Id: Id639fdac8c50036d4c886842f1b515e172284eab

--- msm8974.mk
+++ msm8974.mk
@@ -265,10 +265,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
-# Default to AwesomePlayer
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.use-awesome=true
-
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/oppo/msm8974-common/msm8974-common-vendor.mk)
 
