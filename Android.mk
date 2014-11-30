d09ff457f923e01ddacd418f197ef62e255a073d
YoshiShaPow <YoshiPGA@gmail.com>
2014-11-30 03:25:37 -0500

    bacon : Fix the lunch process

--- Android.mk
+++ Android.mk
@@ -16,7 +16,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),oppo)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
@@ -175,5 +174,4 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
-endif
 endif
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
 add_lunch_combo full_bacon-userdebug
-
