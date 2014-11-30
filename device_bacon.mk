6651b04e631200c6c4ac5e097a52d4fe63f76513
YoshiShaPow <YoshiPGA@gmail.com>
2014-11-30 06:54:28 -0500

    bacon : Use CAF Wi-Fi files

--- device_bacon.mk
+++ device_bacon.mk
@@ -175,8 +175,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     qcmediaplayer
 
-PRODUCT_BOOT_JARS += qcmediaplayer
-
 # NFC
 PRODUCT_PACKAGES += \
     NfcNci \
@@ -218,20 +216,12 @@ PRODUCT_PACKAGES += \
 
 # WiFi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
 PRODUCT_PACKAGES += \
     libwpa_client \
     hostapd \
     wpa_supplicant \
-    wpa_supplicant.conf \
-    wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny
 
 PRODUCT_PACKAGES += \
     wcnss_service
