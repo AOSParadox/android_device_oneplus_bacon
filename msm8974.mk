fc1e7404fa980183b23d99f7366b6c8a03e07ecd
Ilya Katsnelson <k0009000@gmail.com>
2014-12-08 14:43:10 +0300

    oppo/msm8974: disable hardware AAC encoding for now
    Not yet sure what really breaks it, but this fixes a lot of apps that expect AAC encoding, including front camera video.
    Might be worth putting in place for now, until the real issue is solved.
    
    Change-Id: I12765acebacd0d2ff61ba74866f75d17d0cecd01

--- msm8974.mk
+++ msm8974.mk
@@ -69,12 +69,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     use.voice.path.for.pcm.voip=true \
     audio.offload.multiple.enabled=false \
     audio.offload.gapless.enabled=true \
-    qcom.hw.aac.encoder=true \
     tunnel.audio.encode=true \
     media.aac_51_output_enabled=true \
     audio.offload.pcm.enable=true
-
-#    audio.offload.24bit.enable=1
 
 # Charger
 PRODUCT_PACKAGES += \
