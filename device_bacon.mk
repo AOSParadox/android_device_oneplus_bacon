70b51649c8cdfda289908a4b2a52e96b416c24ec
Louis Popi <theh2o64@gmail.com>
2014-12-26 12:30:01 +0100

    bacon : This file doen't exist in CAF

--- device_bacon.mk
+++ device_bacon.mk
@@ -162,7 +162,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Media
 PRODUCT_PACKAGES += \
