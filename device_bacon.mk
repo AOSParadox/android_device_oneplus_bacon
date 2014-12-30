bc8b33cf37251d82a9a4c3c3711a6a5a2a38ea3e
Louis Popi <theh2o64@gmail.com>
2014-12-30 10:05:32 +0100

    bacon : Update from CyanogenMod
    msm8974: Enable IZat
     * DO NOT MERGE, requires additional verification
    
    Change-Id: I0a2906a3ada188571ad1ae1dfa543d3cd60fa3ed
    
    msm8974: Update GPS HAL to LA.BF.1.1_rb1.16
    Change-Id: Iacfcdee2661f1ac8f76813b40d8af6063b23b059
    
    msm8974: Remove VECTORIMPL_SYMBOLS hack
     * Welcome to 1980 br0s
    
    Change-Id: Iae3c2d4926c88740e3fd7282826623d9f263ab80
    
    msm8974: Update bloblist
     * Add tunnel mode encoders and missing GPS library
    
    Change-Id: Ib8a699da33ba324c4c0585a9eff333195e45edfd

--- device_bacon.mk
+++ device_bacon.mk
@@ -111,11 +111,16 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8974 \
     audio.r_submix.default \
     audio.usb.default \
-    libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     tinymix
+
+PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13En
 
 # Charger
 PRODUCT_PACKAGES += \
