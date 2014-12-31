3251fed59f5f3e6d8cfc089646d18a7920cdbb4b
Louis Popi <theh2o64@gmail.com>
2014-12-31 12:49:15 +0100

    baco : Fix "is not a directory"

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -13,10 +13,10 @@ include device/qcom/msm8974/AndroidBoard.mk
 
 #Create symbolic links
 $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/prima; \
-	cp device/oneplus/bacon/wifi/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
+	cp device/oneplus/bacon/wifi/WCNSS_qcom_wlan_nv.bin $(TARGET_OUT_ETC)/firmware/wlan/prima/ \
 	ln -sf /persist/WCNSS_qcom_wlan_nv.bin \
 	$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin; \
-	cp device/oneplus/bacon/wifi/WCNSS_qcom_cfg.ini $(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini \
+	cp device/oneplus/bacon/wifi/WCNSS_qcom_cfg.ini $(TARGET_OUT_ETC)/firmware/wlan/prima/ \
 	ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	$(TARGET_OUT_ETC)/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
