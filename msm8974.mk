3978a5167e0edddbe7f5b6db9d7621d5f4a13ae1
Steve Kondik <steve@cyngn.com>
2014-12-31 00:22:15 -0800

    msm8974: Enable new proximity sensor code
    Change-Id: Iaa84ceea4cfda04df5e3e12f1cd886c35c053708

--- msm8974.mk
+++ msm8974.mk
@@ -249,6 +249,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.telephony.call_ring.multiple=0
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qti.sensors.ir_proximity=true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
