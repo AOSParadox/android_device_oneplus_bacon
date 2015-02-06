9cad03f1217684409bd257a300694de3b863526e
Danny Baumann <dannybaumann@web.de>
2015-02-06 14:59:38 +0100

    Decommonize audio_platform_info.xml (1/3)
    Change-Id: I14f87e66a3547b574989d51b4a0f64b7c77b54d5

--- msm8974.mk
+++ msm8974.mk
@@ -36,7 +36,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/acdb/MTP_Bluetooth_cal.acdb:system/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/MTP_General_cal.acdb:system/etc/acdbdata/MTP/MTP_General_cal.acdb \
     $(LOCAL_PATH)/audio/acdb/MTP_Global_cal.acdb:system/etc/acdbdata/MTP/MTP_Global_cal.acdb \
