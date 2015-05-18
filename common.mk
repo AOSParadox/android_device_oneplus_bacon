minimal-manifest-twrp/platform_device_qcom_common

FRP: Add FRP property & persistent data block
- Set "system" as owner of persistent data block
- Add ro.frp.pst property to PRODUCT_PROPERTY_OVERRIDES

Change-Id: I6751660dfae5d19782922ae597d7366345a6e084

common.mk
@@ -16,7 +16,8 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     persist.radio.apm_sim_not_pwdn=1 \
-    persist.radio.sib16_support=1
+    persist.radio.sib16_support=1 \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 

AOSParadox/android_device_oneplus_bacon

common.mk
@@ -739,7 +739,8 @@ PRODUCT_AAPT_CONFIG += hdpi mdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.radio.apm_sim_not_pwdn=1
+    persist.radio.apm_sim_not_pwdn=1 \
+    ro.frp.pst=/dev/block/bootdevice/by-name/config
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 