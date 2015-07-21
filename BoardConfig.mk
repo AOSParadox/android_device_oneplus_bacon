7b8f01e3f67ef2459114d1370a6d55819516c6ab
Louis Popi <theh2o64@gmail.com>
2015-07-21 14:41:39 +0200

    Partially revert 64914bbef7c9c11f1445d73f49bfa4fe6c55081c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,16 +45,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ARCH := arm
 
-WLAN_MODULES:
-	mkdir -p $(KERNEL_MODULES_OUT)/pronto
-	mkdir -p $(KERNEL_MODULES_OUT)/prima
-	mv $(KERNEL_MODULES_OUT)/pronto_wlan.ko $(KERNEL_MODULES_OUT)/pronto/pronto_wlan.ko
-	mv $(KERNEL_MODULES_OUT)/prima_wlan.ko $(KERNEL_MODULES_OUT)/prima/prima_wlan.ko
-	ln -sf /system/lib/modules/pronto/pronto_wlan.ko $(TARGET_OUT)/lib/modules/pronto_wlan.ko
-	ln -sf /system/lib/modules/prima/prima_wlan.ko $(TARGET_OUT)/lib/modules/prima_wlan.ko
-
-TARGET_KERNEL_MODULES += WLAN_MODULES
-
 # BACON Init
 TARGET_INIT_VENDOR_LIB := libinit_bacon
 
@@ -117,6 +107,7 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Wifi
+# Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_HAS_QCOM_WLAN_SDK          := true
 BOARD_WLAN_DEVICE                := qcwcn
@@ -125,14 +116,11 @@ BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_qcwcn
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/pronto_wlan.ko"
-WIFI_DRIVER_MODULE_PATH          += "/system/lib/modules/prima_wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "pronto_wlan"
-WIFI_DRIVER_MODULE_NAME          += "prima_wlan"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
 TARGET_USES_WCNSS_CTRL           := true
 TARGET_USES_QCOM_WCNSS_QMI       := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
-WLAN_CHIPSET                     := prima
 WLAN_SELECT                      := CONFIG_PRIMA_WLAN=m
 WLAN_SELECT                      += CONFIG_PRONTO_WLAN=m
 
