e004d254ac36d895d69dad0aefc34561dc0224e9
Louis Popi <theh2o64@gmail.com>
2015-07-22 22:22:11 +0200

    bacon : board : Override offset from here

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,8 @@ TARGET_KERNEL_SOURCE := kernel
 KERNEL_DEFCONFIG := cyanogenmod_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ARCH := arm
