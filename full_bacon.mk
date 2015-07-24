4cf1582418faa8c97d25ebd0250ae8f3f1d9cb80
Louis Popi <theh2o64@gmail.com>
2015-07-24 07:58:41 +0200

    bacon : Import AOSParadox vendor

--- full_bacon.mk
+++ full_bacon.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from bacon device
 $(call inherit-product, device/oneplus/bacon/device_bacon.mk)
 
+# Inherit AOSParadox vendor
+$(call inherit-product, vendor/aosparadox/common.mk)
+
 PRODUCT_NAME := full_bacon
 PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
