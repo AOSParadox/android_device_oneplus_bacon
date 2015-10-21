da72541bc524d95d39d21dae1dde7a4154779e6c
Jake Weinstein <xboxlover360@gmail.com>
2015-10-21 05:14:20 +0000

    Revert "Disable dex-preopt"
    This reverts commit 40b8d127d0bcf78e04ec346d3d3d9714707bb52e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,6 +91,19 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 USE_OPENGL_RENDERER := true
+# Enable dex pre-opt to speed up initial boot
+ifneq ($(TARGET_USES_AOSP),true)
+  ifeq ($(HOST_OS),linux)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_PIC := true
+      ifneq ($(TARGET_BUILD_VARIANT),user)
+      # Retain classes.dex in APK's for non-user builds
+      DEX_PREOPT_DEFAULT := nostripping
+      endif
+    endif
+  endif
+endif
 
 TARGET_RECOVERY_FSTAB = device/oneplus/bacon/rootdir/etc/fstab.qcom
 
