50f96d6cd159244e37824ea2aa226b9ce14d6056
Sultanxda <sultanxda@gmail.com>
2015-11-06 20:01:50 -0800

    bacon: power: Use msm_thermal maxfreq node for low-power mode
    Maxfreq for battery-saver mode is controlled via the
    /sys/kernel/msm_thermal/user_maxfreq sysfs node, which is immune to
    hotplugging (so maxfreq changes can be propagated to all CPUs without
    needing to pause mpdecision).
    
    Also clean out everything that we are not using (i.e. the uevent stuff).
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- power/power_bacon.c
+++ power/power_bacon.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,21 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <fcntl.h>
-#include <dlfcn.h>
-#include <cutils/uevent.h>
-#include <errno.h>
-#include <sys/poll.h>
 #include <pthread.h>
-#include <linux/netlink.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define LOG_TAG "PowerHAL"
 #include <utils/Log.h>
@@ -35,23 +26,11 @@
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define MAX_LENGTH			50
-#define UEVENT_MSG_LEN			2048
-#define TOTAL_CPUS			4
-#define RETRY_TIME_CHANGING_FREQ	20
-#define SLEEP_USEC_BETWN_RETRY		200
-#define LOW_POWER_MAX_FREQ		"729600"
-#define NORMAL_MAX_FREQ			"2457600"
-#define UEVENT_STRING			"online@/devices/system/cpu/"
+#define LOW_POWER_MAX_FREQ	"729600"
+#define NORMAL_MAX_FREQ 	"2457600"
 
-static struct pollfd pfd;
-static char *cpu_path_max[] = {
-    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq",
-    "/sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq",
-};
-static bool freq_set[TOTAL_CPUS];
+static const char *cpu_path_max = "/sys/kernel/msm_thermal/user_maxfreq";
+static bool freq_set;
 static bool low_power_mode = false;
 static pthread_mutex_t low_power_mode_lock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -78,102 +57,9 @@ static int sysfs_write(const char *path, char *s)
     return 0;
 }
 
-static int uevent_event()
-{
-    char msg[UEVENT_MSG_LEN];
-    char *cp;
-    int n, cpu, ret, retry = RETRY_TIME_CHANGING_FREQ;
-
-    n = recv(pfd.fd, msg, UEVENT_MSG_LEN, MSG_DONTWAIT);
-    if (n <= 0) {
-        return -1;
-    }
-    if (n >= UEVENT_MSG_LEN) {   /* overflow -- discard */
-        return -1;
-    }
-
-    cp = msg;
-
-    if (strstr(cp, UEVENT_STRING)) {
-        n = strlen(cp);
-        errno = 0;
-        cpu = strtol(cp + n - 1, NULL, 10);
-
-        if (errno == EINVAL || errno == ERANGE || cpu < 0 || cpu >= TOTAL_CPUS) {
-            return -1;
-        }
-
-        pthread_mutex_lock(&low_power_mode_lock);
-        if (low_power_mode && !freq_set[cpu]) {
-            while (retry) {
-                ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
-                if (!ret) {
-                    freq_set[cpu] = true;
-                    break;
-                }
-                usleep(SLEEP_USEC_BETWN_RETRY);
-                retry--;
-           }
-        } else if (!low_power_mode && freq_set[cpu]) {
-             while (retry) {
-                  ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
-                  if (!ret) {
-                      freq_set[cpu] = false;
-                      break;
-                  }
-                  usleep(SLEEP_USEC_BETWN_RETRY);
-                  retry--;
-             }
-        }
-        pthread_mutex_unlock(&low_power_mode_lock);
-    }
-    return 0;
-}
-
-void *thread_uevent(__attribute__((unused)) void *x)
-{
-    while (1) {
-        int nevents, ret;
-
-        nevents = poll(&pfd, 1, -1);
-
-        if (nevents == -1) {
-            if (errno == EINTR)
-                continue;
-            ALOGE("powerhal: thread_uevent: poll_wait failed\n");
-            break;
-        }
-        ret = uevent_event();
-        if (ret < 0)
-            ALOGE("Error processing the uevent event");
-    }
-    return NULL;
-}
-
-static void uevent_init()
-{
-    struct sockaddr_nl client;
-    pthread_t tid;
-    pfd.fd = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
-
-    if (pfd.fd < 0) {
-        ALOGE("%s: failed to open: %s", __func__, strerror(errno));
-        return;
-    }
-    memset(&client, 0, sizeof(struct sockaddr_nl));
-    pthread_create(&tid, NULL, thread_uevent, NULL);
-    client.nl_family = AF_NETLINK;
-    client.nl_pid = tid;
-    client.nl_groups = -1;
-    pfd.events = POLLIN;
-    bind(pfd.fd, (void *)&client, sizeof(struct sockaddr_nl));
-    return;
-}
-
 static void power_init(__attribute__((unused)) struct power_module *module)
 {
     ALOGI("%s", __func__);
-    uevent_init();
 }
 
 static void power_set_interactive(__attribute__((unused)) struct power_module *module,
@@ -192,19 +78,15 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
     pthread_mutex_lock(&low_power_mode_lock);
     if (data) {
         low_power_mode = true;
-        for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-            ret = sysfs_write(cpu_path_max[cpu], LOW_POWER_MAX_FREQ);
-            if (!ret) {
-                freq_set[cpu] = true;
-            }
+        ret = sysfs_write(cpu_path_max, LOW_POWER_MAX_FREQ);
+        if (!ret) {
+            freq_set = true;
         }
     } else {
         low_power_mode = false;
-        for (cpu = 0; cpu < TOTAL_CPUS; cpu++) {
-            ret = sysfs_write(cpu_path_max[cpu], NORMAL_MAX_FREQ);
-            if (!ret) {
-                freq_set[cpu] = false;
-            }
+        ret = sysfs_write(cpu_path_max, NORMAL_MAX_FREQ);
+        if (!ret) {
+            freq_set = false;
         }
     }
     pthread_mutex_unlock(&low_power_mode_lock);
@@ -237,7 +119,7 @@ struct power_module HAL_MODULE_INFO_SYM = {
         .methods = &power_module_methods,
     },
     .init = power_init,
-    .setInteractive = power_set_interactive,
     .powerHint = power_hint,
+    .setInteractive = power_set_interactive,
     .setFeature = set_feature
 };
