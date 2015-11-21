14d0d2f3ed66b0fd5749bd6035e97a80f9cfd8e4
Louis Popi <theh2o64@gmail.com>
2015-11-21 14:00:50 +0100

    bacon: Vendor upstream updates https://github.com/CyanogenMod/android_device_oneplus_bacon

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The AOSParadox Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,10 @@
 # limitations under the License.
 #
 
+ifneq ($(filter bacon,$(TARGET_DEVICE)),)
 
 LOC_PATH := $(call my-dir)
 
-#include $(call first-makefiles-under,$(LOC_PATH))
+include $(call first-makefiles-under,$(LOC_PATH))
 
+endif
