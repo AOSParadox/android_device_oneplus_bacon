dd12dc2bf8bdc44a61b1aa543786db2ccd45f3ed
maxwen <max.weninger@gmail.com>
2015-12-14 00:30:02 +0100

    bacon: kernel level hw button disable
    use the kernel sysfs to completely disable reporting
    of hw buttons
    
    Change-Id: Iff0671dbe9baf24fee75998f7b017d3bee5f2564
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- Find7Parts/src/org/omnirom/device/KeyHandler.java
+++ Find7Parts/src/org/omnirom/device/KeyHandler.java
@@ -5,6 +5,7 @@ import android.app.KeyguardManager;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,6 +20,7 @@ import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.WindowManagerGlobal;
@@ -41,6 +43,8 @@ public class KeyHandler implements DeviceKeyHandler {
     private static final int GESTURE_GTR_SCANCODE = 254;
     private static final int KEY_DOUBLE_TAP = 255;
 
+    private static final String BUTTON_DISABLE_FILE = "/sys/kernel/touchscreen/button_disable";
+
     private static final int[] sSupportedGestures = new int[]{
         GESTURE_CIRCLE_SCANCODE,
         GESTURE_V_SCANCODE,
@@ -55,6 +59,30 @@ public class KeyHandler implements DeviceKeyHandler {
     private EventHandler mEventHandler;
     private WakeLock mGestureWakeLock;
     private KeyguardManager mKeyguardManager;
+    private Handler mHandler = new Handler();
+    private SettingsObserver mSettingsObserver;
+
+    private class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.HARDWARE_KEYS_DISABLE),
+                    false, this);
+            update();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            update();
+        }
+
+        public void update() {
+            setButtonDisable(mContext);
+        }
+    }
 
     public KeyHandler(Context context) {
         mContext = context;
@@ -62,6 +90,8 @@ public class KeyHandler implements DeviceKeyHandler {
         mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
         mGestureWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
                 "GestureWakeLock");
+        mSettingsObserver = new SettingsObserver(mHandler);
+        mSettingsObserver.observe();
     }
 
     private void ensureKeyguardManager() {
@@ -116,6 +146,7 @@ public class KeyHandler implements DeviceKeyHandler {
         }
     }
 
+    @Override
     public boolean handleKeyEvent(KeyEvent event) {
         if (event.getAction() != KeyEvent.ACTION_UP) {
             return false;
@@ -164,6 +195,13 @@ public class KeyHandler implements DeviceKeyHandler {
         } catch (ActivityNotFoundException e) {
             // Ignore
         }
+    }
+
+    public static void setButtonDisable(Context context) {
+        final boolean disableButtons = Settings.System.getInt(
+                context.getContentResolver(), Settings.System.HARDWARE_KEYS_DISABLE, 0) == 1;
+        if (DEBUG) Log.i(TAG, "setButtonDisable=" + disableButtons);
+        Utils.writeValue(BUTTON_DISABLE_FILE, disableButtons ? "1" : "0");
     }
 }
 
--- Find7Parts/src/org/omnirom/device/Startup.java
+++ Find7Parts/src/org/omnirom/device/Startup.java
@@ -29,6 +29,6 @@ public class Startup extends BroadcastReceiver {
         CameraGestureSwitch.restore(context);
         TorchGestureSwitch.restore(context);
         MusicGestureSwitch.restore(context);
-	//KeyDisabler.restore(context);
+        KeyHandler.setButtonDisable(context);
     }
 }
