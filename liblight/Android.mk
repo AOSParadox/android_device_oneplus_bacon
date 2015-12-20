464e48ab62158cec1d868e348b9900ba0061baf5
maxwen <max.weninger@gmail.com>
2015-12-20 15:19:01 +0100

    bacon: Update liblight
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2008 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,7 +19,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_SHARED_LIBRARIES := liblog \
+	libcutils
 
 LOCAL_MODULE := lights.msm8974
 LOCAL_MODULE_TAGS := optional
