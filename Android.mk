10c4c40528863b1b314ce32645781f277229d8a0
Sultanxda <sultanxda@gmail.com>
2016-01-03 10:33:47 -0800

    Revert "bacon: Remove mba/modem symlinks to modem partition"
    This reverts commit dc1e273733db3c50e146e3fe7fcdf600013f99d5.

--- Android.mk
+++ Android.mk
@@ -22,6 +22,34 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
+MODEM_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
+    modem.b06 modem.b07 modem.b08 modem.b09 modem.b10 modem.b11 \
+    modem.b12 modem.b13 modem.b14 modem.b15 modem.b16 modem.b17 \
+    modem.b18 modem.b19 modem.b20 modem.b21 modem.b22 modem.b23 \
+    modem.b24 modem.b25 modem.b26 modem.b27 modem.mdt
+
+MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_IMAGES)))
+$(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
+
+MBA_IMAGES := \
+    mba.b00 mba.mdt
+
+MBA_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MBA_IMAGES)))
+$(MBA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "MBA firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(MBA_SYMLINKS)
+
 # Create links for audcal data files
 $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wcd9320; \
 	ln -sf /data/misc/audio/wcd9320_anc.bin \
