0c043e367bb2fac09afd19f8a0747c7e24fdd189
Louis Popi <theh2o64@gmail.com>
2016-02-21 20:08:50 +0100

    bacon: Run sepolicy permissive for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_OTA_ASSERT_DEVICE := bacon,A0001
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
 KERNEL_DEFCONFIG := bacon_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 androidboot.selinux=permissive
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
 TARGET_USE_CM_RAMDISK := true
